772e39f8dfaa4e65d10aa89ed07630e1c3af6a36
Pedro Chazanas <chazanaspln@gmail.com>
2024-04-18 20:34:04 -0300

    python 2 version

--- docker_img_individual/source/main.py
+++ docker_img_individual/source/main.py
@@ -2,8 +2,8 @@ import numpy as np
 from tqdm import tqdm
 
 num_sum = 0
-number_of_randoms = 1e6
+number_of_randoms = int(1e6)
 for _ in tqdm(range(number_of_randoms)):
     num_sum += np.random.randint(10, 1000)
-print(num_sum / number_of_randoms)
+print num_sum / number_of_randoms
 
